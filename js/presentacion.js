333778b6cc73fdf2fe0fbb310f51a84feb17a705
belulopezz <belenlopeza@hotmail.com>
2020-10-25 23:49:07 -0300

    se agregó audio a presentación

--- js/presentacion.js
+++ js/presentacion.js
@@ -157,3 +157,23 @@ alfombra.style.backgroundColor = "#A03D1D"
 alfombra.width = 1000
 alfombra.heigth= 595
 
+/* Audio */
+
+var boton = document.getElementById("botonAi");
+var sound2 = document.getElementById ("sound2");
+var v = document.getElementsByTagName ("audio")[0];
+var sound = true;
+
+
+boton.addEventListener("click", function (){
+  if(sound == true){ 
+    v.pause();
+    sound2.src ="https://img.icons8.com/carbon-copy/100/000000/mute.png";
+    sound = false;
+  }else {
+    v.play();
+    sound2.src = "https://img.icons8.com/carbon-copy/100/000000/low-volume.png";
+    sound = true;
+  }
+
+});
