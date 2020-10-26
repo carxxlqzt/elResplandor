c1c993e3329599967f5f076a3a427d23157b776d
morascatalanas <catamoragas@hotmail.com>
2020-10-26 11:26:45 -0300

    fondopresentacion

--- js/presentacion.js
+++ js/presentacion.js
@@ -149,12 +149,14 @@ hexagonoA(0,450)
 fistLine(1,500)
 secondLine(140,560)
 
-var imgConverted = document.querySelector('#imgConverted')
-var dataURL = canvas.toDataURL();
-imgConverted.src = dataURL;
-var alfombra = document.querySelector('#alfombra')
-alfombra.style.backgroundColor = "#A03D1D"
-alfombra.width = 1000
-alfombra.heigth= 595
+function pegar(){
+  var imgConverted = document.querySelectorAll('.imgConverted')
+  for (var i = 0; i < imgConverted.length; i++){
+  var dataURL = canvas.toDataURL();
+  imgConverted[i].src = dataURL;
+  }
+}
+ pegar()  
+
 
 
